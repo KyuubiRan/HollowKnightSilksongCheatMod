3a2db14bf5b7d24bc672c1f3182d4369c8c2ead6
Kitsune <KyuubiRan@outlook.com>
2025-09-13 00:32:06 +0800

    fix: noclip issue

--- src/Features/Player/NoclipFeature.cs
+++ src/Features/Player/NoclipFeature.cs
@@ -11,31 +11,13 @@ namespace HKSC.Features.Player;
 public class NoclipFeature : FeatureBase
 {
     private static HeroController Hc => HeroController.UnsafeInstance;
-    
+    private static InputHandler Ih => InputHandler.UnsafeInstance;
+
     public override ModPage Page => ModPage.Player;
-    
+
     public readonly ConfigObject<bool> IsEnabled = CfgManager.Create("Noclip::Enable", false)
-        .CreateToggleHotkey("Toggle Noclip")
-        .AddOnChangedListener(x =>
-        {
-            if (Hc == null) return;
-            
-            Hc.AffectedByGravity(!x);
-            var rigidbody2D = HeroControllerAccessor.Rb2dField(Hc);
-            var collider2D = HeroControllerAccessor.Col2dField(Hc);
-            if (x)
-            {
-                rigidbody2D.velocity = Vector2.zero;
-                rigidbody2D.isKinematic = true;
-                collider2D.enabled = false;
-            }
-            else
-            {
-                rigidbody2D.isKinematic = false;
-                collider2D.enabled = true;
-            }
-        });
-    
+        .CreateToggleHotkey("Toggle Noclip");
+
     public readonly ConfigObject<float> Speed = CfgManager.Create("Noclip::Speed", 5f);
 
     protected override void OnGui()
@@ -46,18 +28,47 @@ public class NoclipFeature : FeatureBase
         {
             Speed.Value = UiUtils.Slider(Speed, 5f, 100f, 5f, valueFormat: "Speed: {0:0.0}");
         }
+
         UiUtils.EndCategory();
     }
 
+    private Rigidbody2D _rigidbody2D;
+    private Collider2D _collider2D;
+
     protected override void OnUpdate()
     {
-        if (Hc == null)
+        if (Hc == null || Ih == null)
             return;
-        
-        if (!IsEnabled)
-            return;
-        
-        var move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        _rigidbody2D ??= HeroControllerAccessor.Rb2dField(Hc);
+        _collider2D ??= HeroControllerAccessor.Col2dField(Hc);
+
+        if (IsEnabled)
+        {
+            _rigidbody2D.velocity = Vector2.zero;
+            _rigidbody2D.isKinematic = true;
+            _collider2D.enabled = false;
+        }
+        else
+        {
+            _rigidbody2D.isKinematic = false;
+            _collider2D.enabled = true;
+        }
+
+        if (!IsEnabled) return;
+
+        var vertical = 0f;
+        var horizontal = 0f;
+        if (Ih.inputActions.Up.IsPressed || Ih.inputActions.RsUp.IsPressed)
+            vertical = 1f;
+        else if (Ih.inputActions.Down.IsPressed || Ih.inputActions.RsDown.IsPressed)
+            vertical = -1f;
+        if (Ih.inputActions.Left.IsPressed || Ih.inputActions.RsLeft.IsPressed)
+            horizontal = -1f;
+        else if (Ih.inputActions.Right.IsPressed || Ih.inputActions.RsRight.IsPressed)
+            horizontal = 1f;
+
+        var move = new Vector2(horizontal, vertical);
         if (move.sqrMagnitude > 0.01f)
         {
             move.Normalize();
