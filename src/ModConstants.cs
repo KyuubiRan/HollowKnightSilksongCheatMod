a752e7ab22e86e30cd9cb39317a8a41112b89764
Kitsune <KyuubiRan@outlook.com>
2025-10-01 19:32:54 +0800

    chore: bump version to 1.1.3

--- src/ModConstants.cs
+++ src/ModConstants.cs
@@ -4,7 +4,7 @@ internal static class ModConstants
 {
     internal const string Namespace = "io.github.kyuubiran.hksc.mod";
     internal const string ModName = "Hollow Knight Silksong Cheat";
-    internal const string Version = "1.1.2";
+    internal const string Version = "1.1.3";
     internal const string Author = "KyuubiRan";
     internal const string ProjectSource = "https://github.com/KyuubiRan/HollowKnightSilksongCheatMod";
 }
